388087b27ec16f84548d3bfaeeb6b395b19b41c6
sopium <sopium@users.noreply.github.com>
2017-01-11 08:38:15 +0800

    build-static: generate tarball

--- build-static.sh
+++ build-static.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Copyright 2017 Sopium
 
 # This file is part of TiTun.
@@ -15,19 +17,32 @@
 # You should have received a copy of the GNU General Public License
 # along with TiTun.  If not, see <https://www.gnu.org/licenses/>.
 
-#!/bin/sh
-
 # Need musl-gcc.
 
 set -e
+
+function cleanup {
+    rm -rf titun
+    rm -rf libsodium-1.0.11
+    rm -f libsodium-1.0.11.tar.gz
+}
+
+trap cleanup EXIT
 
 wget https://github.com/jedisct1/libsodium/releases/download/1.0.11/libsodium-1.0.11.tar.gz
 tar xf libsodium-1.0.11.tar.gz
 cd libsodium-1.0.11
 CC=musl-gcc ./configure --disable-shared
 make -j8
+cd ..
 
 export SODIUM_LIB_DIR=$PWD/src/libsodium/.libs/
 export SODIUM_STATIC=1
 
 cargo build --release --target x86_64-unknown-linux-musl
+mkdir titun
+cp target/x86_64-unknown-linux-musl/release/titun titun/titun
+cp README.md titun/
+cp COPYING titun/
+cp contrib/* titun/
+tar acf titun.tar.gz titun/
